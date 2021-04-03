efa40ee69b550898179d405950b431deaa86a9dd
Mikun Hatsune <47102682+Hatsune-Mikun@users.noreply.github.com>
2021-04-03 19:43:18 +0300

    6ой урок

---
+++ 6. v-model.trim, form/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <SignUp />
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+  color: #474747;
+}
+
+h1, h2, h3, h4 {
+  font-weight: 700;
+  color: #333333;
+  margin-top: 0;
+}
+
+.flex {
+  display: flex;
+}
+
+.list-default {
+  padding-left: 0;
+  margin-bottom: 0;
+  list-style: none;
+}
+
+.list-default > li {
+  font-size: 16px;
+  margin-bottom: 14px;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+  margin-top: 40px;
+}
+</style>
---
+++ 6. v-model.trim, form/src/components/SignUp.vue
@@ -0,0 +1,106 @@
+<template>
+  <form class="sign-up">
+    <div class="form-group">
+      <label for="login">Логин:</label>
+      <input id="login" class="form-control" v-model.trim="form.login">
+    </div>
+    <div class="form-group">
+      <label for="login">Почта:</label>
+      <input id="email" type="email" class="form-control" v-model.trim="form.email">
+    </div>
+    <div class="form-group">
+      <label for="login">Пароль:</label>
+      <input id="password" type="password" class="form-control" v-model.trim="form.password">
+    </div>
+    <div class="form-group">
+      <label for="country">Страна проживания:</label>
+      <select id="country" class="form-control" v-model="form.country">
+        <option v-for="(country, index) in countries" :value="country.value" :key="index">{{country.label}}</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="themes">Любимые темы:</label>
+      <select id="themes" class="form-control" v-model="form.favouriteThemes" multiple>
+        <option v-for="(theme, index) in themes" :value="theme.value" :key="index">{{theme.label}}</option>
+      </select>
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" value="1" class="form-check-input" id="notification" v-model="form.agreeWithSendToMail">
+      <label class="form-check-label" for="notification">Уведомлять меня о новых курсах</label>
+    </div>
+    <div class="form-group form-check">
+      <input type="checkbox" value="2" class="form-check-input" id="notification2" v-model="form.agreeWithSendToMail">
+      <label class="form-check-label" for="notification2">Уведомлять меня о новых курсах</label>
+    </div>
+    <div class="flex">
+      <div class="form-check">
+        <input class="form-check-input" type="radio" value="male" name="exampleRadios" id="male" v-model="form.gender">
+        <label class="form-check-label" for="male">Мужчина</label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" value="female" name="exampleRadios" id="female" v-model="form.gender">
+        <label class="form-check-label" for="female">Женщина</label>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Сохранить</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      form: {
+        login: '',
+        email: '',
+        password: '',
+        country: 'Russia',
+        gender: 'male',
+        favouriteThemes: ['IT'],
+        agreeWithSendToMail: [],
+      },
+      countries: [
+        {
+        label: 'Россия',
+        value: 'Russia'
+        }, 
+        {
+          label: 'Украина',
+          value: 'Ukraine'
+        },
+        {
+          label: 'США',
+          value: 'USA'
+      }],
+      themes: [
+        {
+          label: 'Технологии',
+          value: 'IT'
+        },
+        {
+          label: 'Языки',
+          value: 'languages'
+        },
+        {
+          label: 'Математика',
+          value: 'mathematics'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.form-control {
+  width: 400px;
+}
+.form-check {
+  margin-right: 10px;
+}
+button {
+  margin-top: 15px;
+}
+</style>
