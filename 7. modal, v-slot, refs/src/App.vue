954157e9f6f1e718929f5cdd98c16aee32861268
Mikun Hatsune <47102682+Hatsune-Mikun@users.noreply.github.com>
2021-04-05 21:08:53 +0300

    Залила 7ой урок

---
+++ 7. modal, v-slot, refs/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <userPage />
+</template>
+
+<script>
+import userPage from './components/userPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    userPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
